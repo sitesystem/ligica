c33a16f505a8fdf57ee4ba2582926fecbd27d48c
sitesystem <l_aloaxa05@hotmail.com>
2022-02-05 12:23:53 -0600

    Actualización

---
+++ Curso Python/HolaMundo/HolaMundo.py
@@ -0,0 +1,11 @@
+# Comentario: Ejemplo Hola Mundo
+# Los comentarios sirven para documentar y aclarar nuestro código
+# Los omite o no los toma en cuenta en la ejecución del programa
+
+print(1+2)
+print("Hola Mundo")
+print("Eduardo González")
+
+print('Imprimir Texto')
+print("""Texto 1""")
+print('''Texto 2''')
